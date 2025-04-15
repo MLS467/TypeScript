a4e327eb0721ebb25b1f4e5ee845f2e82ede1342
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-04-14 22:17:43 -0300

    Aliases e Interface

---
+++ src/modules/aliaseseInterface.ts
@@ -0,0 +1,7 @@
+/***
+ * Aliases - são nomes alternativos para os módulos, 
+ * permitindo que você use nomes mais curtos ou mais significativos em vez
+ * de nomes de módulos longos ou complexos.
+ */
+
+// ALIASES (criando nosso tipos personalizados)
