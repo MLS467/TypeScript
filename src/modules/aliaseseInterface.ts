862137ef2bb16997b7d522c1b3e2608ee2b4861a
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-04-14 22:35:41 -0300

    Aliases e Interface

--- src/modules/aliaseseInterface.ts
+++ src/modules/aliaseseInterface.ts
@@ -5,3 +5,16 @@
  */
 
 // ALIASES (criando nosso tipos personalizados)
+type AnoVeiculo = number;
+type MarcaVeiculo = string;
+type ModeloVeiculo = string;
+
+type Carro = {
+    ano: AnoVeiculo;
+    marca: MarcaVeiculo;
+    modelo: ModeloVeiculo;
+}
+
+const uno: Carro = { ano: 2020, marca: "Fiat", modelo: "Uno" }
+const palio: Carro = { ano: 2018, marca: "Fiat", modelo: "Palio" }
+const Ferrari: Carro = { ano: 2021, marca: "Ferrari", modelo: "v8" }
